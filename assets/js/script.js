24b6ca0c388de995aad9032f6d7b6b0d469718cf
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-08-30 21:39:01 -0400

    working hour-by-hour layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,50 @@
 
 $(document).ready(function () {
     var mainContainer = $(".container");
+    var currentDayEl = $("#currentDay");
     var hourDivEl = $("<div>").addClass("row time-block");
     var hourLabelEl = $("<div>").addClass("col-md-1 hour");
-    var hourDescriptionEl = $("<div>").addClass("col-md-10 description");
+    var hourDescriptionEl = $("<textarea>").addClass("col-md-10 description");
     var saveButtonEl = $("<button>").addClass("btn saveBtn col-md-1");
 
+    var jumbotronDate = moment().format("dddd DD MMMM, YYYY");
+
+    var nowHour = moment().hour()
 
 
     function refreshDay() {
 
+        mainContainer.empty();
+        console.log("Current hour: " + nowHour);
+
         // loop troush the hours of the day to create grid
         for (i = 8; i < 19; i++) {
-            var hourDivID = "hour-" + i;
-            hourLabelEl.text(`${i}`);
+            var myHour = moment().hour(i).format("HH:00");
+            var hourDivID = "hour-" + myHour;
+            hourLabelEl.text(`${myHour}`);
             hourDivEl.attr("id", hourDivID).append(hourLabelEl, hourDescriptionEl, saveButtonEl);
+            if (i < nowHour) {
+                hourDivEl.attr("class", "row time-block past");
+            } else if (nowHour === i) {
+                hourDivEl.attr("class", "row time-block present");
+            } else {
+                hourDivEl.attr("class", "row time-block future");
+            }
             mainContainer.append(hourDivEl.clone());
+
         }
     }
 
-    $(".saveBtn").on("click", function () {
-        console.log("Button Click");
+
+    $(document).on("click", ".saveBtn", function () {
+        console.log($(this));
         refreshDay();
+
+
     });
+
+    currentDayEl.text(jumbotronDate);
+
     refreshDay();
 
 });
